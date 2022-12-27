fe1624d139255aa44ae255ffb6fc1e5fa13d6154
Sai Charan Reddy <saicharanreddy1999.ct@gmail.com>
2022-12-27 19:08:37 +0530

    bug

--- src/util/api.js
+++ src/util/api.js
@@ -1,9 +1,7 @@
-<<<<<<< HEAD
+// <<<<<<< HEAD
 export const host = 'https://student-teacher.vercel.app/';
 
 // export const host = "http://localhost:4000"
-=======
+// =======
 
-export const host = "https://student-teacher.vercel.app"
 
->>>>>>> 50b4db4b24ad6a02743616cfbc2847bfcd820ca9
