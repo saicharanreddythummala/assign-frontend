1c6433a6fb4d111f27207f94ec3d8fc364b440d1
Sai Charan Reddy <saicharanreddy1999.ct@gmail.com>
2022-12-27 19:11:49 +0530

    host

--- src/util/api.js
+++ src/util/api.js
@@ -1,5 +1,5 @@
 // <<<<<<< HEAD
-export const host = 'https://student-teacher.vercel.app/';
+export const host = 'https://student-teacher.vercel.app';
 
 // export const host = "http://localhost:4000"
 // =======
